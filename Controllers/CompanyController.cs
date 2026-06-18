01f07704b30133225fdba71a7ac0b3124b017473
Vinay Samanthula <vinay.samanthula@avcitsolutions.com>
2026-06-18 20:37:07 +0530

    Complete Job Tracking System

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,91 @@
+﻿using JobTrackingSystem.Data;
+using JobTrackingSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTrackingSystem.Controllers
+{
+    [Authorize]
+    public class CompanyController : Controller
+    {
+        private readonly JobTrackingSystemContext _context;
+
+        public CompanyController(JobTrackingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var companies = await _context.Companies.ToListAsync();
+            return View(companies);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Company company)
+        {
+            if (!ModelState.IsValid)
+                return View(company);
+
+            _context.Companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company == null)
+                return NotFound();
+
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Company company)
+        {
+            if (!ModelState.IsValid)
+                return View(company);
+
+            _context.Update(company);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company == null)
+                return NotFound();
+
+            return View(company);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company != null)
+            {
+                _context.Companies.Remove(company);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- Models/Company.cs
+++ Models/Company.cs
@@ -1,12 +1,12 @@
-﻿namespace JobTrackingSystem.Models;
+﻿    namespace JobTrackingSystem.Models;
 
-public class Company
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public string Website { get; set; }
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Website { get; set; }
 
-    public ICollection<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
+        public ICollection<JobApplication> JobApplications { get; set; } = new List<JobApplication>();
 
-}
+    }
